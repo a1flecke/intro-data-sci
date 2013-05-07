f3731455b0ca33f6b8faed6ce38d5fa36c364f22
Aaron Fleckenstein <aaron.fleckenstein@daveramsey.com>
2013-05-07 12:28:12 -0500

    finished 1.2, 1.3

--- term_sentiment.py
+++ term_sentiment.py
@@ -1,17 +1,47 @@
 import sys
-
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+import re
+import json
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+  sent_file = open(sys.argv[1])
+  tweet_file = open(sys.argv[2])
+
+  sentiments = loadSentiment(sent_file)
+  tweets = loadTweets(tweet_file)
+
+  for tweet in tweets:
+    calculateSentiment(tweet, sentiments)
+
+def loadSentiment(fp):
+  sent = {}
+
+  for line in fp:
+    line_split = re.split(r'(\t+)',line)
+    sent[line_split[0]] = int(line_split[2])
+
+  return sent
+
+def loadTweets(fp):
+  tweets = []
+  for line in fp:
+    tweet = json.loads(line)
+    if u'text' in tweet:
+      tweets.append(tweet[u'text'])
+
+  return tweets
+
+def calculateSentiment(tweet, sentiments):
+  tweet_split = tweet.split()
+  sent_sum = 0
+  for tweet_word in tweet_split:
+    if tweet_word in sentiments:
+      sent_sum += sent_sum + int(sentiments[tweet_word])
+
+  for tweet_word in tweet_split:
+    if tweet_word in sentiments:
+      print tweet_word, int(sentiments[tweet_word])
+    else:
+      print tweet_word, sent_sum
 
 if __name__ == '__main__':
     main()
--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -13,27 +13,36 @@ def main():
     tweet_file = open(sys.argv[2])
 
     sentiments = loadSentiment(sent_file)
-
     tweets = loadTweets(tweet_file)
 
-    #for tweet in tweets:
-     ##print tweet_sentiment
+    for tweet in tweets:
+      print calculateSentiment(tweet, sentiments)
 
 def loadSentiment(fp):
   sent = {}
 
   for line in fp:
-    line_split = re.split(r'(\s+)',line)
-    sent[line_split[0]] = line_split[2]
+    line_split = re.split(r'(\t+)',line)
+    sent[line_split[0]] = int(line_split[2])
 
   return sent
 
 def loadTweets(fp):
+  tweets = []
   for line in fp:
     tweet = json.loads(line)
     if u'text' in tweet:
-      print tweet[u'text'].encode('utf-8')
+      tweets.append(tweet[u'text'])
 
+  return tweets
+
+def calculateSentiment(tweet, sentiments):
+  tweet_split = re.split(r'(\s+)',tweet)
+  sent_sum = 0
+  for tweet_word in tweet_split:
+    if tweet_word in sentiments:
+      sent_sum += sent_sum + int(sentiments[tweet_word])
+  return sent_sum
 
 if __name__ == '__main__':
     main()
